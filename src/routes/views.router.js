0e822687f3a8ec9184aeebf42b1d2f020245736f
Martin Fueyo <martin.fueyo@hotmail.com>
2023-08-24 14:26:16 -0300

    PrimeraPruebaIntegradora

---
+++ src/routes/views.router.js
@@ -0,0 +1,19 @@
+import express from "express";
+import ProductManager from "../dao/ProductManager";
+
+const router = express.Router();
+const PM = new ProductManager();
+
+router.get("/", async (req, res) => {
+    const products = await PM.getProducts();
+    res.render("home", {products});
+});
+
+router.get("/realtimeproducts", (req, res) => {
+    res.render("realTimeProducts");
+});
+
+router.get("/chat", (req, res) => {
+    res.render("chat");
+});
+
